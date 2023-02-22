d6e1fe9cbee4f5d03ff4c3728b0c6c58edd3d14f
Mojid Eroje <93765911+Eroje@users.noreply.github.com>
2023-02-22 13:34:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
 else {
-  document.querySelector("h1").innerHTML = "Draw!!!";
+  document.querySelector("h1").innerHTML = "Draw💥💥💥";
 }
